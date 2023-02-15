fdb8685baa837d1c261d33deebb86a683b274f07
kimyw2357 <bubuchachakk@gmail.com>
2023-02-16 00:12:49 +0900

    react youtube start

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
